312605e767237f1b9ec5c22a20335a608fc25828
Deepak Dey <deepakdey412@gmail.com>
2025-07-16 18:47:38 +0530

    Add files via upload

---
+++ Factory_design_pattern/MissCallNotification.java
@@ -0,0 +1,8 @@
+package Factory_design_pattern;
+
+public class MissCallNotification implements Notification{
+
+    public void notifyUser() {
+        System.out.println("Miss-call Notification");
+    }
+}
