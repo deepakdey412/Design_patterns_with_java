74f0a091a04f4377cecfcd5613fc72717bf78dcc
Deepak Dey <deepakdey412@gmail.com>
2025-07-16 18:47:20 +0530

    Add files via upload

---
+++ Factory_design_pattern/EmailNotification.java
@@ -0,0 +1,7 @@
+package Factory_design_pattern;
+
+public class EmailNotification implements Notification{
+    public void notifyUser() {
+        System.out.println("Email Notification");
+    }
+}
